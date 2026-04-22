ac97ff62875cd9bb83c4c20de4531efacbbbe95a
Gandharv <123283645+GKProgrammer@users.noreply.github.com>
2026-04-22 19:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error, mean_absolute_error
 st.set_page_config(layout="wide", page_title="Global Inflation Analytics")
 # --- Data Loading ---
 @st.cache_data
